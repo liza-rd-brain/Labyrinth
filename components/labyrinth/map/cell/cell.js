1395b9c3da1060206a03d87a99640fa1377ba259
MinakovaLisa <Giti528@ya.ru>
2018-04-08 21:09:20 +0300

    Рисуется карта. Рандмное заполнение ячеек.

---
+++ components/labyrinth/map/cell/cell.js
@@ -0,0 +1,84 @@
+function Cell(col, row, blockSize, ctx) {
+    this.col = col
+    this.row = row
+    this.blockSize = blockSize
+    this.ctx = ctx
+    this.drawCircle = this.drawCircle.bind(this)
+    this.x = null
+    this.y = null
+
+}
+
+
+let ceprt = Cell.prototype
+ceprt.CLASS_NAME = "cell"
+
+ceprt.landscape = ["ground", "grass", "water"]
+
+
+ceprt.render = function () {
+    let cellEl = document.createElement("div")
+    cellEl.className = this.CLASS_NAME
+    cellEl.innerHTML = "кусь"
+    this.drawGrid("red")
+    this.drawLandscape()
+}
+
+
+//рисуем сетку карты
+ceprt.drawGrid = function (color) {
+    this.x = this.col * this.blockSize
+    this.y = this.row * this.blockSize
+    this.ctx.strokeStyle = color
+    this.ctx.strokeRect(this.x, this.y, this.blockSize, this.blockSize)
+}
+
+ceprt.drawLandscape = function () {
+
+    let pickLandscape = this.landscape[Math.floor(Math.random() * this.landscape.length)]
+    this.ctx.fillText(pickLandscape, this.x + this.blockSize / 3, this.y + this.blockSize / 3)
+
+    let landscapeColor
+    /* let landscapeColor(pickLandscape) */
+    switch (pickLandscape) {
+        case "ground":
+            landscapeColor = "rgb(128,0,0, 0.5)"
+            break;
+        case "grass":
+            landscapeColor = "rgb( 0, 255, 0, 0.5)"
+            break;
+        case "water":
+            landscapeColor = "	rgb(0,0,255, 0.5)"
+            break;
+
+    }
+
+    this.ctx.fillStyle = landscapeColor
+    this.ctx.fillRect(this.x, this.y, this.blockSize, this.blockSize)
+
+
+
+}
+
+
+//функция рисования кружочка
+ceprt.circle = function (x, y, radius, fillCircle) {
+    this.ctx.beginPath()
+    this.ctx.arc(x, y, radius, 0, Math.PI * 2, false)
+    if (fillCircle) {
+        this.ctx.fill()
+    } else {
+        this.ctx.stroke()
+    }
+}
+
+//нарисовать кружочек
+ceprt.drawCircle = function (color) {
+    var centerX = this.col * this.blockSize + this.blockSize / 2
+    var centerY = this.row * this.blockSize + this.blockSize / 2
+    this.ctx.fillStyle = color
+    this.circle(centerX, centerY, this.blockSize / 2, true)
+}
+
+
+ceprt = null
--- components/labyrinth/map/map.js
+++ components/labyrinth/map/map.js
@@ -1,4 +1,7 @@
 function Map() {
+    this.mapEl = null
+    this.ctx = null
+    this.blockSize = null
 }
 
 let mprt = Map.prototype
@@ -7,26 +10,53 @@ mprt.CLASS_NAME = "map"
 //ПОКА ОТДАМ КАРТУ ЭЛЕМЕНТУ HUMAN
 
 mprt.render = function () {
-    let mapEl = document.createElement("div")
-    mapEl.className = this.CLASS_NAME
 
+    this.mapEl = document.createElement("div")
+    this.mapEl.className = this.CLASS_NAME
 
-    let map = []
+    this.makeCanvas()
+    this.makeMapChild()
 
-    let n = 6
-    let m = 4
-
-    for (i = 0; i < n; i++) {
-        map[i] = document.createElement("div")
-        /*map[i].className = "mapCell"*/
-        /*mapEl.appendChild(map[i])*/
-        for (j = 0; j < m; j++) {
-            map[i][j] = document.createElement("div")
-            map[i][j].className = "mapCell"
-            mapEl.appendChild(map[i][j])
-        }
+    /* //рисуем ограждение карты
+    var drawBorder = function () {
+        ctx.fillStyle = "Gray"
+        ctx.fillRect(0, 0, width, 1)
+        ctx.fillRect(0, height - 1, width, 1)
+        ctx.fillRect(0, 0, 1, height)
+        ctx.fillRect(width - 1, 0, 1, height)
     }
-    return mapEl
+    drawBorder() */
+
+
+    return this.mapEl
+
 }
 
+mprt.makeCanvas = function () {
+    //канва
+    var canvas = document.getElementById("canvas")
+    this.ctx = canvas.getContext("2d")
+    this.mapEl .appendChild(canvas)
+
+    // Получаем ширину и высоту элемента canvas
+    var width = canvas.width
+    var height = canvas.height
+    //высота и ширина в блоках
+    this.blockSize = 50
+    var widthInBlocks = width / this.blockSize
+    var heightInBlocks = height / this.blockSize
+}
+
+//рисуем на канве кубики (кружочки)
+mprt.makeMapChild = function () {
+    let n = 6
+    let m = 4
+    let mapCell = new Array(n)
+    for (i = 0; i < n; i++) {
+        mapCell[i] = new Array(m)
+        for (j = 0; j < m; j++) {
+            mapCell[i][j] = new Cell(i, j, this.blockSize, this.ctx).render()/* drawCell("red") */
+        }
+    }
+}
 mprt = null
